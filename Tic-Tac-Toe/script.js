980d6498956d51a03c6467d7d7b5a2b69cde96c3
Alfa06N <nicolasalfag@gmail.com>
2024-03-17 20:08:34 -0400

    Completa el proyecto Tic-Tac-Toe

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -3,63 +3,174 @@ let playerTwo;
 
 // objects / function factory
 
-// myGameBoard IIFE
-const myGameBoard = (() => {
-    const gameBoardFactory = () => {
-        let board;
+const interface = (() => {
+    let board;
 
-        // Crea el tablero
-        const createBoard = () => {
-            const rows = 3;
-            const columns = 3;
-            board = document.querySelector('.grid');
+    // Crea el tablero:
 
-            // Borra el contenido existente del div .grid
-            board.innerHTML = '';
+    const createBoard = () => {
+        board = interface.getGrid();
 
-            // Usa for para crear una matriz 2D
-            for (let i = 0; i < rows; i++) {
-                // Crea las filas
-                const row = document.createElement('div');
-                row.classList.add('row');
+        board.innerHTML = '';
 
-
-                for (let j = 0; j < columns; j++) {
-                    // Crea las columnas
-                    const cell = document.createElement('div');
-                    cell.classList.add('column');
-                    cell.textContent = '';
-                    row.appendChild(cell);
-                }
-
-                // Agrega cada row al board
-                board.appendChild(row);
-            }
-            return board;
+        // Crea las cuadrículas
+        for (let i = 0; i < 9; i++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('gridSquare');
+            board.appendChild(gridSquare);
         }
-
-        // Obtiene el valor actual del tablero
-        const getBoard = () => {
-            return board;
-        }
-
-        // Marca una casilla del tablero
-        const setCellValue = (row, column, value) => {
-            if (row >= 0 && row < board.length && column >= 0 && column < board.length) {
-                board[row][column] = value;
-            } else {
-                console.error('Invalid row or column index.');
-            }
-            return board;
-        }
-
-        return { createBoard, getBoard, setCellValue }; 
     }
 
-    return gameBoardFactory();
+    const validInput = () => {
+        let inputOne = document.querySelector('.inputOne').value;
+        let inputTwo = document.querySelector('.inputTwo').value;
+
+        if (inputOne.trim() !== '' && inputTwo.trim() !== '') {
+            return { inputOne, inputTwo }
+        }
+        return false;
+    }
+
+    const getGrid = () => {
+        const myGrid = document.querySelector('.grid');
+        return myGrid;
+    }
+
+    const getStartButton = () => {
+        const button = document.querySelector('.button.start');
+        return button;
+    }
+
+    const showInputPlayers = () => {
+        let beginning = document.querySelector('.beginning');
+        let overlay = document.querySelector('.overlay');
+
+        beginning.style.display = 'flex';
+        overlay.style.display = 'block';
+    }
+        
+    const closeInputPlayers = () => {
+        let beginning = document.querySelector('.beginning');
+        let overlay = document.querySelector('.overlay');
+
+        let inputOne = document.querySelector('.inputOne');
+        let inputTwo = document.querySelector('.inputTwo');
+
+        inputOne.value = '';
+        inputTwo.value = '';
+
+        beginning.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+
+    const showGrid = () => {
+        interface.getGrid().style.display = 'grid';
+    }
+
+    const getFightButton = () => {
+        const button = document.querySelector('.button.accept');
+
+        return button;
+    }
+
+    const showPlayers = () => {
+        let title = document.querySelector('.showPlayers');
+
+        if (control.getCurrentPlayer() === playerOne) {
+            title.innerHTML = `<span>${playerOne.playerName()} (${playerOne.playerSymbol()})</span> VS ${playerTwo.playerName()} (${playerTwo.playerSymbol()})`;
+        } else {
+            title.innerHTML = `${playerOne.playerName()} (${playerOne.playerSymbol()}) VS <span>${playerTwo.playerName()} (${playerTwo.playerSymbol()})</span>`;
+        }
+    }
+
+    const showWinner = () => {
+        const final = document.querySelector('.final');
+        const overlay = document.querySelector('.overlay');
+
+        final.style.display = 'flex';
+        overlay.style.display = 'block';
+    }
+
+    const closeWinner = () => {
+        const final = document.querySelector('.final');
+        const overlay = document.querySelector('.overlay');
+        final.style.display = 'none';
+        overlay.style.display = 'none';
+
+        const title = document.querySelector('.winner');
+        const titleDialog = document.querySelector('.titleDialog');
+
+        titleDialog.textContent = 'El ganador es';
+        title.textContent = '';
+    }
+
+    const getRebootButton = () => {
+        return document.querySelector('.reboot');
+    }
+
+    const getExitButton = () => {
+        return document.querySelector('.cancel');
+    }
+
+    // addEventListener()
+
+    const initEventListeners = () => {
+        interface.getStartButton().addEventListener('click', function (event) {
+            event.preventDefault();
+            interface.showInputPlayers();
+        });
+
+        interface.getFightButton().addEventListener('click', (event) => {
+            event.preventDefault();
+            control.startGame();
+        });
+
+        document.querySelector('.exit').addEventListener('click', (event) => {
+            event.preventDefault();
+            interface.closeInputPlayers();
+        })
+
+        document.querySelector('.exit').addEventListener('click', (event) => {
+            event.preventDefault();
+
+            interface.closeInputPlayers();
+        })
+
+        interface.getGrid().addEventListener('click', function (event) {
+            event.preventDefault();
+            let target = event.target;
+
+            if (target.textContent === '') {
+                // Marca la casilla
+                let player = control.getCurrentPlayer();
+                player.markGrid(target);
+
+                // Verifica el ganador
+                if (control.verifyWinner() === false) {
+                    // Cambia el turno del jugador
+                    control.switchPlayer();
+                    interface.showPlayers();
+                }
+            }
+        })
+
+        interface.getRebootButton().addEventListener('click', (event) => {
+            event.preventDefault()
+            interface.closeWinner();
+            control.rebootGame();
+        })
+
+        interface.getExitButton().addEventListener('click', (event) => {
+            event.preventDefault();
+            interface.closeWinner();
+            control.backHome();
+        })
+    }
+        
+    return { createBoard, validInput, getGrid, getStartButton, showInputPlayers, showGrid, getFightButton, closeInputPlayers, showPlayers, showWinner, closeWinner, getRebootButton, getExitButton, initEventListeners };
+
 })();
 
-// Player factory
 const player = (name, symbol) => {
     const playerName = () => {
         return name;
@@ -69,214 +180,130 @@ const player = (name, symbol) => {
         return symbol;
     }
 
-    const markGrid = (symbol) => {
-        // Obtiene las celdas del grid
-        const cells = document.querySelectorAll('.column');
-
-        // Agrega un evento a cada una:
-        cells.forEach((cell, index) => {
-            cell.addEventListener('click', () => {
-                // El índice de la fila es el índice de la fila entre 3.
-                const row = Math.floor(index / 3);
-
-                // El índice de la columna es el índice dividido entre 3
-                const column = index % 3;
-
-                const move = control.isValidMove(cell);
-
-                if (move) {
-                    // Marcar la celda
-                    cell.textContent = symbol;
-
-                    console.log({ row, column, symbol});
-                }
-            })
-        });
+    const markGrid = (target) => {
+        let symbol = control.getCurrentPlayer().playerSymbol();
+        if (target.textContent === '') {
+            target.textContent = symbol;
+            target.classList.add(symbol);
+        }
     }
 
-    return { playerName, playerSymbol, markGrid };
+    return { playerName, playerSymbol, markGrid }
 }
 
-// control IIFE
 const control = (() => {
-    const flowGame = () => {
 
-        // Función para inicializar el juego
-        const startGame = () => {
-            let inputPlayers = interfaceControl.validInput();
-            console.log(inputPlayers);
+    let currentPlayer;
 
-            if (inputPlayers !== false) {
-                interfaceControl.closeInputPlayers();
-                // Crear instancias de los jugadores:
-                playerOne = player(inputPlayers.inputOne, 'X');
-                playerTwo = player(inputPlayers.inputTwo, 'O');
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    }
 
-                myGameBoard.createBoard();
+    // Inicializa el juego
+    const startGame = () => {
+        let inputPlayers = interface.validInput();
+        console.log(inputPlayers)
+
+        if (inputPlayers !== false) {
+            interface.closeInputPlayers();
+            // Crear instancias de los jugadores:
+            console.log(inputPlayers.inputOne);
+            console.log(inputPlayers.inputTwo);
+
+            playerOne = player(inputPlayers.inputOne, 'X');
+            playerTwo = player(inputPlayers.inputTwo, 'O');
+
+            interface.createBoard();
+            interface.showGrid();
+            currentPlayer = playerOne;
+
+            interface.getStartButton().style.display = 'none';
+            interface.showPlayers();
+        }
+    }
+
+    const switchPlayer = () => {
+        if (control.getCurrentPlayer().playerSymbol() === 'X') {
+            console.log('Turno del jugador:', playerTwo.playerName());
+            currentPlayer = playerTwo;
+        } else {
+            console.log('Turno del jugador:', playerOne.playerName());
+            currentPlayer = playerOne;
+        }
+    }
+
+    const verifyWinner = () => {
+        const cells = document.querySelectorAll('.gridSquare');
+
+        const combos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Row
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        let isBoardFull = true;
+
+        for (const cell of cells) {
+            if (cell.textContent === '') {
+                isBoardFull = false;
+                break;
             }
-            
 
-            let currentPlayer = playerOne;
-            gameLogic(currentPlayer);
         }
 
-        // Variable para verificar si alguien ha ganado
-        const verifyBoard = () => {
-            const board = myGameBoard.getBoard();
+        if (isBoardFull) {
+            const winner = document.querySelector('.winner');
+            const title = document.querySelector('.titleDialog');
+            title.textContent = 'No hay ningún ganador';
+            winner.textContent = 'Empatados';
+            interface.showWinner();
+        } else {
+            for (const combo of combos) {
+                const [a, b, c] = combo; // posisiones
+                const cellA = cells[a].textContent;
+                const cellB = cells[b].textContent;
+                const cellC = cells[c].textContent;
 
-            // Verificar las filas:
-            for (let i = 0; i < 3; i++) {
-                if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][2]) {
-                    return board[i][0];
-                }
-            }
-
-            // Verificar las columnas:
-            for (let i = 0; i < 3; i++) {
-                if (board[0][i] !== '' && board[0][i] === board[1][i] && board[2][i]) {
-                    return board[0][i];
-                }
-            }
-
-            // Verifica diagonal izquierda a derecha
-            if (board[0][0] !== '' && board[0][0] === board[1][1] && board[2][2]) {
-                return board[0][0];
-            }
-
-            // Verifica diagonal derecha a izquierda
-            if (board[0][2] !== '' && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-                return board[0][2];
-            }
-
-            return null;
-        }
-
-        // Variable para declarar el ganador
-        const declareWinner = (winner) => {
-            console.log('Congratulations! The winner is', winner);
-        }
-
-        const printBoard = () => {
-            let board = myGameBoard.getBoard();
-            console.log('Estado actual del tablero:');
-            console.table(board);
-        }
-
-        const gameLogic = (currentPlayer) => {
-            // Obtiene la jugada del currentPlayer
-            currentPlayer.markGrid(currentPlayer.playerSymbol());
-
-            if (isValidMove(move)) {
-                // Marcar casilla
-                myGameBoard.setCellValue(move.row, move.column, currentPlayer.playerSymbol());
-                console.log(`${currentPlayer.playerName()} has marked X in the position ${move.row}-${move.column}.`)
-
-                if (verifyBoard() === 'X') {
-                    declareWinner(currentPlayer.playerName());
-                    return null;
-                } else if (verifyBoard() === 'O') {
-                    declareWinner(currentPlayer.playerName());
-                    return null;
-                }
-
-                // Alternar el turno del jugador
-                currentPlayer = switchPlayer(currentPlayer);
-
-                // Volvemos a llamar a gameLogic si aún no hay ganador
-                gameLogic(currentPlayer);
-            }
-        }
-
-            const switchPlayer = (currentPlayer) => {
-                if (currentPlayer.playerSymbol() === 'X') {
-                    return playerTwo;
-                } else {
-                    return playerOne;
-                }
-            }
-
-            const isValidMove = (cell) => {
-                if (cell.textContent === '') {
+                if (cellA !== '' && cellA === cellB && cellA === cellC) {
+                    control.declareWinner();
                     return true;
                 }
-                console.log('Esta celda está llena');
-                return false;
             }
-        return { startGame, verifyBoard, declareWinner, gameLogic, isValidMove, printBoard };
-    }
-    return flowGame;
-})();
-
-// functions
-
-const interfaceControl = (() => {
-    const interfaceFunctions = () => {
-
-        const getStartButton = () => {
-            const button = document.querySelector('.button.start');
-            return button
         }
 
-        // Mostrar beginning dialog
-        const showInputPlayers = () => {
-            let beginning = document.querySelector('.beginning');
-            let overlay = document.querySelector('.overlay');
-
-            beginning.style.display = 'flex';
-            overlay.style.display = 'block';
-            
-        }
-
-        const getFightButton = () => {
-            const button = document.querySelector('.button.accept');
-
-            return button;
-        }
-
-        const closeInputPlayers = () => {
-            let beginning = document.querySelector('.beginning');
-            let overlay = document.querySelector('.overlay');
-
-            beginning.style.display = 'none';
-            overlay.style.display = 'none';
-
-            interfaceControl.grid().style.display = 'flex';
-        }
-
-        const grid = () => {
-            const myGrid = document.querySelector('.grid');
-            return myGrid;
-        }
-
-        const validInput = () => {
-            let inputOne = document.querySelector('.inputOne').value;
-            let inputTwo = document.querySelector('.inputTwo').value;
-
-            if (inputOne !== '' && inputTwo !== '') {
-                
-
-                return { inputOne,  inputTwo }
-            }
-            return false;
-        }
-
-        return { showInputPlayers, getStartButton, getFightButton, validInput, closeInputPlayers, grid };
+        return false;
     }
 
-    return interfaceFunctions();
+    const declareWinner = () => {
+        const winner = control.getCurrentPlayer();
+
+        const title = document.querySelector('.winner');
+
+        title.textContent = winner.playerName();
+        interface.showWinner();
+    }
+
+    const rebootGame = () => {
+        interface.closeWinner();
+        interface.createBoard();
+        currentPlayer = playerOne;
+        interface.showPlayers();
+    }
+
+    const backHome = () => {
+        interface.getStartButton().style.display = 'block';
+        interface.getGrid().style.display = 'none';
+        document.querySelector('.overlay').style.display = 'none';
+        document.querySelector('.showPlayers').style.display = 'none';
+        currentPlayer = null;
+    }
+
+    return { getCurrentPlayer, startGame, switchPlayer, verifyWinner, declareWinner, rebootGame, backHome };
+    
 })();
 
 // addEventListener
 
-interfaceControl.getStartButton().addEventListener('click', function (event) {
-    event.preventDefault();
-
-    interfaceControl.showInputPlayers();
-
-})
-
-interfaceControl.getFightButton().addEventListener('click', (event) => {
-    event.preventDefault();
-
-    control.startGame();
+document.addEventListener('DOMContentLoaded', () => {
+    interface.initEventListeners();
 })
